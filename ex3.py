0d8c1ea51dc4f01fccde793fe6cdbe597829cf0a
AvrahamBi <avrahambarilan@gmail.com>
2021-12-10 16:13:54 +0200

    Base code finished

--- ex3.py
+++ ex3.py
@@ -8,20 +8,35 @@ FIRST_LAYER_SIZE = 150
 LAST_LAYER_SIZE = 10
 ETA = 0.005
 #EPOCHS = 50
-EPOCHS = 3
+EPOCHS = 50
 MAX_COLOR = 255
 TRAIN_PERCENT = 0.8
 
+sigmoid = lambda x: 1 / (1 + np.exp(-x))
+
+def relu(x):
+    return np.maximum(np.zeros(np.shape(x)), x)
+def relu_derivative(x):
+    x[x > 0] = 1
+    x[x <= 0] = 0
+    return x
 
 def status(msg):
     now = datetime.now()
     print(msg, "\t\t",  now.strftime("%H:%M:%S.%f"))
 
+def softmax(x):
+    exps = np.exp(x)
+    sum_exps = np.sum(exps)
+    # to make sure we don't divide by 0
+    if sum_exps == 0:
+        sum_exps = 0.001
+    return exps / sum_exps
+
 class NN:
     def __init__(self):
         self.initData()
         self.initParams()
-
 
     def initParams(self):
         self.sigmoid = lambda x: 1 / (1 + np.exp(-x))
@@ -43,23 +58,6 @@ class NN:
         # self.train_x = shuffled_train_x
         # self.train_y = shuffled_train_y
 
-
-    def relu(self):
-        pass
-
-    def softmax(self):
-        pass
-
-    def predict_y_hat(self, image):
-        image = shape(IMAGE_SIZE, 1)
-        z1 = np.dot(w1, image) + b1
-        h1 = self.relu(z1)
-        z2 = np.dot(w2, h1) + b2
-        h2 = self.softmax(z2)
-        return np.argmax(h2)
-
-
-
     # Back propagation, Forward propagation
     def train(self):
         status("Training began")
@@ -71,31 +69,61 @@ class NN:
             loss = []
             for image, digit in zip(train_x, train_y):
                 image = np.divide(image, MAX_COLOR)
+                #
                 # forward prop
+                #
                 image = image.reshape(IMAGE_SIZE, 1)
                 z1 = np.dot(self.w1, image) + self.b1
                 h1 = relu(z1)
+                #h1 = sigmoid(z1)
                 z2 = np.dot(self.w2, h1) + self.b2
                 h2 = softmax(z2)
                 predictionsVector = np.zeros(LAST_LAYER_SIZE)
                 #  maybe need to remove np.int
-                predictionsVector[np.int(digit)] = 1
+                predictionsVector[int(digit)] = 1
+                #
                 # back prop
+                #
+                # maybe need to split (l. 90)
+                predictionsVector = np.asarray(predictionsVector).reshape(LAST_LAYER_SIZE, 1)
 
+                dl_dz2 = np.subtract(h2, predictionsVector)
+                dl_dw2 = np.dot(dl_dz2, np.transpose(h1))
+                dl_db2 = dl_dz2
+                dl_dh1 = np.dot(np.transpose(self.w2), dl_dz2)
+                #dl_dz1 = np.dot(self.w2.T, (dz2)) * sigmoid(z1) * (1 - sigmoid(z1))
+                dl_dz1 = np.multiply(dl_dh1, relu_derivative(z1))
+                dl_dw1 = np.dot(dl_dz1, np.transpose(image))
+                dl_db1 = dl_dz1
                 # update params
+                self.w1 = np.subtract(self.w1, np.multiply(dl_dw1, ETA))
+                self.b1 = np.subtract(self.b1, np.multiply(dl_db1, ETA))
+                self.w2 = np.subtract(self.w2, np.multiply(dl_dw2, ETA))
+                self.b2 = np.subtract(self.b2, np.multiply(dl_db2, ETA))
+
+    def predict_y_hat(self, image):
+        image.shape = (IMAGE_SIZE, 1)
+        z1 = np.dot(self.w1, image) + self.b1
+        h1 = relu(z1)
+        z2 = np.dot(self.w2, h1) + self.b2
+        h2 = softmax(z2)
+        return np.argmax(h2)
 
 
-
-
-
-
-
+    def test(self):
+        status("Test started")
+        # array that will store all predictions - this will be the output in the test_y.txt file
+        predictions_arr = []
+        for image in self.test_x:
+            image = np.divide(image, MAX_COLOR)
+            predictions_arr.append(int(self.predict_y_hat(image)))
+        return predictions_arr
 
 
 if __name__ == '__main__':
     status("Started")
     nn = NN()
     nn.train()
-
-
+    predictions = nn.test()
+    np.savetxt("test_y", predictions, fmt='%d', delimiter='\n')
     status("Finish")
